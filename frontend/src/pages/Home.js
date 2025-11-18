e9d635a905f0eb0bcdcecad238aa07493fae00d0
Omar <omarnasmi@gmail.com>
2025-11-18 18:02:39 +0100

    adding frontend

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to the Warehouse Management System</h1>
+            <p>Manage your inventory efficiently and effectively.</p>
+        </div>
+    );
+}
+export default Home;
